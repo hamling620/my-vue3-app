fdfe56b81b80b99a54ebae1d94f9ef8b2d86d47f
lch0l03 <lch0l03@sp.noahgroup.com>
2021-04-09 17:22:41 +0800

    update

--- src/doc_study/provide&inject/index.vue
+++ src/doc_study/provide&inject/index.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
+    <ISwitch v-model:value="isLight" @input="handleChange" />
     主题色 {{ themeValue }}
     <Toolbar />
   </div>
 </template>
 
 <script>
-import { ref, provide } from 'vue'
+import { ref, watch, provide } from 'vue'
+import ISwitch from '@/components/ISwitch'
 import Toolbar from './Toolbar'
 // Provide & Inject (提供/注入) 类似于react context
 // 父组件通过provide选项来提供数据，子组件有一个inject选项来使用这个数据
 // 父组件不需要知道哪些子组件使用它提供的property，子组件不需要知道inject property来自哪里
 export default {
   components: {
-    Toolbar
+    Toolbar,
+    ISwitch
   },
+  emits: ['update:value'],
   setup () {
     const themeValue = ref('light')
+    const isLight = ref(true)
+    watch(themeValue, (value) => {
+      isLight.value = value === 'light'
+    })
     const handleChange = () => {
       themeValue.value = themeValue.value === 'light' ? 'dark' : 'light'
     }
@@ -25,7 +33,9 @@ export default {
       handleChange
     })
     return {
-      themeValue
+      themeValue,
+      isLight,
+      handleChange
     }
   }
 }
